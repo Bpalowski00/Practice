14df4752a542c4713f5e630a3d104f5f331d6ef9
b.palowski <brianpalowski00@gmail.com>
2019-03-04 13:56:57 -0800

    updated

---
+++ ProblemSolving/Part1.js
@@ -0,0 +1,32 @@
+// Function
+// Write a function that takes in a string and returns the cound of each characater 
+
+/// Understand the problem_______________________________
+//input string OUTPUT how many of char kind is in the string number
+
+// Examples_______________________________
+// Simple 
+// INPUT  hello OUTPUT {h:1,e:1,l:2,o:1};
+// INPUT  hello123 OUTPUT {h:1,e:1,l:2,o:1,1:1,2:1,3:1};
+
+// More complex concrete examples
+
+//INPUT  "hello my name is212" OUTPUT ?
+//INPUT  "hello my" OUTPUT ?
+//INPUT  "!?#" OUTPUT ?
+//INPUT  "HhbB" OUTPUT ?
+//INPUT "empty string" OUTPUT ?
+
+// Break It down_______________________________
+// Take steps you need to take.
+
+// function charCount(str){
+// // Do something
+// // return
+// }
+
+function charCount(str) {
+	// Do something
+	// return object with keys that are lower case alphanumeric characters
+	// values should be the number of times character shows up
+}
